9ac7a0fef6141aec291014a65e693a3c99bf4331
PH <paulofera159@gmail.com>
2025-02-22 13:02:23 -0300

    Introdução de funcionalidades

--- main.py
+++ main.py
@@ -2,6 +2,26 @@
 
 from randomize import randomize
 from letters import letters
-from verify import verify
+from verify import verify, process_atempt
 
 def hangman_game():
+    palavra = randomize()
+    letras_corretas = set()
+    letras_incorretas = set()
+    numero_tentativas = 6
+
+    print("Bem-vindo ao Jogo da Forca! 🚪")
+
+    while numero_tentativas > 0:
+        print("/nPalavra: ", letters(palavra, letras_corretas))
+        print(f"Letras corretas: {', '.join(letras_corretas) if letras_corretas else "Nenhuma"}")
+        print(f"Letras incorretas: {', '.join(letras_incorretas) if letras_incorretas else "Nenhuma"}")
+        print(f"Tentativas Restantes: {numero_tentativas}")
+
+        tentativa = input(str("Escreva uma letra: ")).lower()
+
+        if (len(tentativa) > 1 or tentativa == ''):
+            print("Você pode escrever somente 1 letra!")
+            break   
+
+        
