bb176e2f8105a4cfbc7a690a9940a2dd1c40078b
PH <paulofera159@gmail.com>
2025-02-25 17:26:07 -0300

    Atualização da lista

--- src/randomize.py
+++ src/randomize.py
@@ -4,7 +4,15 @@ import random
 
 def randomize():
 
-    palavras = ["maçã", "banana", "laranja", "morango", "kiwi","sol","lua","programação","casa"," escola"," bairro" "escritor", "acampamento","lógica","borboleta","livro","celular","computador","praça","tela","cabelo","país","sábado","semana","tablet","bolo", "escrito","literatura","caderno","música","site","vampiro","frutas","açai","sal","shopping","igreja","etapas","vida","ciclo", "massa","regra","deletar","amor","aleatoriamente","alfa","cachorro","gato","carro","moto","cidade","professores","curso", "celular","aplicativos","palavras","pessoas","inteligentes","beleza","natureza","escolaridae","profissionais","jogos","natal", "comunidade","escolhida","sentimento","parque","árvore","criança","belo","dia","felicidades","amigos","artificial","valores", "casamento","cantoria","seleção","causa","matemática","física","aplicativo","moradia","papelaria","python","java","espaço","livre", "liberdade","fama","riqueza","amigos","colegas","escritores","livraria","conjuntos","navegação","filmes","pesquisar","luz","nascimento", "morada","estudos","redação","mouse","fotos","aprovado","data","nublado","trabalho","noivado","namoro","passeio","férias","aula"]
+    palavras = ["cebola", "banana", "laranja", "morango", "kiwi","sol","lua","ouvido","casa"," escola"," bairro","escritor",
+    "acampamento","nome","borboleta","livro","celular","computador","linguagem","tela","cabelo","estado","domingo","semana","tablet","bolo",
+    "escrito","literatura","caderno","mais","site","vampiro","frutas","caju","sal","shopping","igreja","etapas","vida","ciclo",
+    "massa","regra","deletar","amor","aleatoriamente","alfa","cachorro","gato","carro","moto","cidade","professores","curso",
+    "celular","aplicativos","palavras","pessoas","inteligentes","beleza","natureza","escolaridae","profissionais","jogos","natal",
+    "comunidade","escolhida","sentimento","parque","planta","criança","belo","dia","felicidades","amigos","artificial","valores",
+    "casamento","cantoria","print","causa","filosofia","geografia","aplicativo","moradia","papelaria","python","java","espaço","livre",
+    "liberdade","fama","riqueza","amigos","colegas","escritores","livraria","conjuntos","navio","filmes","pesquisar","luz","nascimento",
+    "morada","estudos","aberto","mouse","fotos","aprovado","data","nublado","trabalho","noivado","namoro","passeio","galinha","aula"]
 
     palavra_aleatoria = random.choice(palavras)
     
